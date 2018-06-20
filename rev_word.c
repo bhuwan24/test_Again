c2ee989439f43770d6290fd92ca053d6093f49c8
manish <tiwari.manish409@gmail.com>
2018-06-20 09:51:42 +0530

    It is my first commit

---
+++ rev_word.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+
+main()
+{
+	int i,j,k;
+	char s[25],temp;
+	printf(" enter string\n");
+	gets(s);
+	printf(" string is = %s\n",s);
+
+	for(i=0,k=0;;i++)
+	{
+		if((s[i]==' ')||(s[i]=='\0'))
+		{
+			for(j=i-1;j>k;j--,k++)
+			{
+				temp=s[k];
+				s[k]=s[j];
+				s[j]=temp;
+			}
+			k=i+1;
+			if(s[i]=='\0')
+				break;
+		}
+	}
+	printf(" %s\n",s);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
